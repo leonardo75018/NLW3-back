d7d4520b8f76c15db95cd2c6862522fc327d961f
Kabongo <leonardo.kabongo@gmail.com>
2023-09-12 17:07:39 +0200

    implemented error gestion

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { NextFunction, Request, Response, json } from 'express'
+import 'express-async-errors'
 import cors from 'cors'
 import router from './views/index'
 import path from 'path'
@@ -9,5 +10,19 @@ app.use(express.urlencoded({ extended: true }), cors(), express.json(), router)
 //Config express url to show image in browser
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 const PORT = 3000
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message
+      })
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error '
+    })
+  }
+)
 
 app.listen(PORT, () => console.log(`serveur run on port ${PORT}`))
